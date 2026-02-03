c2c13887bf0e0b2055ebddf7998f50995b1f1e9e
Lovely304-q <lovelysoni7808@gmail.com>
2026-02-03 10:40:32 +0530

    Add 6th sem

---
+++ 6th sem/day-02/fetchpi.js
@@ -0,0 +1,30 @@
+// fetch("https://fakestoreapi.com/products")
+// .then((res)=>res.json())
+// .then((data)=> console.log(data))
+// .catch((error)=>
+//     console.log("some error occured while fetching the data....."))
+const div=document.getElementById("products");
+const btn=document.getElementById("btn");
+console.log(btn);
+const getData =async () => {
+    try{
+        const res =await fetch("https://fakestoreapi.com/products");
+        const data = await res.json();
+        return data;
+    }
+    catch(error)
+    {
+        return"unable to fetch the data";
+    }
+}
+btn.addEventListener("click",async(e) =>
+{
+    e.preventDefault();
+    const data =await getData();
+    data.map(d)  => {
+        const insideDiv = document.createElement("div");
+        insideDiv.innerHTML =d.title;
+        div.appendChild(insideDiv);
+    }
+
+})
